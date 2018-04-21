10f357c5f9014ba1f5e1b31f0af009b81b38a474
Pamela Santos <pamesantoss@hotmail.com>
2018-04-21 17:48:21 -0300

    Adicionando Browser Sync para atualizar automaticamente a página

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var browserSync = require('browser-sync').create();
 
 // Testando se funciona
 /*gulp.task('default', function(){
@@ -28,7 +29,8 @@ var SassProdOptions = {
 gulp.task('sassdev', function(){
 	return gulp.src(scssFiles)
 		.pipe(sass(SassDevOptions).on('error', sass.logError))
-		.pipe(gulp.dest(cssDest));
+		.pipe(gulp.dest(cssDest))
+		.pipe(browserSync.stream());
 });
 
 // Sass para produção - Run with command 'gulp sassprod'
@@ -36,12 +38,18 @@ gulp.task('sassprod', function(){
 	return gulp.src(scssFiles)
 		.pipe(sass(SassProdOptions).on('error', sass.logError))
 		.pipe(rename('style.min.css'))
-		.pipe(gulp.dest(cssDest));
+		.pipe(gulp.dest(cssDest))
+		.pipe(browserSync.stream());
 });
 
 // Criando task para 'gulp watch'
 gulp.task('watch', function(){
+	browserSync.init({
+        server: "./"
+    });
+
 	gulp.watch(scssFiles, ['sassdev', 'sassprod']);
+	gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 // Default task - Run with command 'gulp'
